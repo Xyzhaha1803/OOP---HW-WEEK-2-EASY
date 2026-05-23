1ec763cb4ce8eaba7e9837ff9095cd2867ed4364
Xyzhaha1803 <38728814+Xyzhaha1803@users.noreply.github.com>
2026-05-23 05:30:58 +0000

    Question 2

---
+++ Q2.java
@@ -0,0 +1,14 @@
+/*Simple arithmetic operations.Write a Java program that declares 
+two integers and prints their sum, difference, product, and quotient. */
+
+public class Q2{
+    public static void main(String[] args){
+        int num1 = 50;
+        int num2 = 100;
+
+        System.out.println("Sum: " + (num1+num2));
+        System.out.println("Difference: " + (num1-num2));
+        System.out.println("Product: " + (num1*num2));
+        System.out.println("Quotient: " + (num1/num2));
+    }
+}
