cf5e6b8d81001cbaee67f3b93a0ea4dc611d7ce0
Xyzhaha1803 <38728814+Xyzhaha1803@users.noreply.github.com>
2026-05-23 05:18:52 +0000

    Question 1

---
+++ Q1.java
@@ -0,0 +1,18 @@
+/*Create and print variables. Write a Java program that declares an integer, 
+a double, and a string. Assign them values and print them. */
+
+public class Q1{
+    public static void main(String[] args){
+        int a;
+        double b;
+        String c;
+
+        a = 10;
+        b = 99.99;
+        c = "Hello World!";
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("String: " + c);
+    }
+}
